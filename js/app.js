a0fb7682ec160fbab0fcee447b7145f5e8137416
= <=>
2013-06-28 12:05:17 +0100

    tracking touch event, need to get canvas resizing to mobile browser window size

--- js/app.js
+++ js/app.js
@@ -45,3 +45,78 @@ function drawMarkers(){
 }
 
 
+
+function initTouch(){
+
+    console.log("init touch");
+    canvas.addEventListener("mousedown", this.mouseDown, false);
+    canvas.addEventListener("touchstart", this.touchDown, false);
+    canvas.addEventListener("touchmove", draw, false);
+    canvas.addEventListener("touchmove", this.touchXY, true);
+    canvas.addEventListener("touchend", this.touchUp, false);
+
+    document.body.addEventListener("mouseup", this.mouseUp, false);
+    document.body.addEventListener("touchcancel", this.touchUp, false);
+
+}
+
+function mouseMove(event){
+  var coors = {
+      x: event.targetTouches[0].pageX,
+      y: event.targetTouches[0].pageY
+   };
+
+}
+function mouseUp() {
+          mouseIsDown = 0;          
+          
+}
+
+function draw(event){
+
+
+  console.log("draw......");
+   // get the touch coordinates
+   var coors = {
+      x: event.targetTouches[0].pageX,
+      y: event.targetTouches[0].pageY
+   };
+ 
+  //context.clearRect(0,0,canvasWidth, canvasHeight);
+  drawCanvasBG();
+  //draw a circle
+
+  context.beginPath();
+  context.arc(coors.x, coors.y, 20, 0, 2 * Math.PI, false);
+  context.fillStyle = 'black';
+  context.fill();
+  context.lineWidth = 5;
+  context.strokeStyle = '#000000';
+  context.stroke();
+
+
+
+  context.stroke();
+   // pass the coordinates to the appropriate handler
+//   drawer[event.type](coors);
+}
+
+
+function touchUp() {
+            mouseIsDown = 0;
+            // no touch to track, so just show state
+          //  this.showPos();
+}
+
+function mouseDown() {
+      console.log("md");
+      mouseIsDown = 1;
+          //  this.mouseXY();
+
+}
+
+function touchDown() {
+            mouseIsDown = 1;
+ //           this.touchXY();
+}
+
