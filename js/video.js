ded251d6009feede4b75593445e8f5726d8f1938
dhrubo <dhrubo@gmail.com>
2013-07-01 13:59:45 +0100

    button touchevent added 2

--- js/video.js
+++ js/video.js
@@ -5,9 +5,10 @@
             $(function(){
                 // hide video
                 $("#myVideo").css({'visibility' : 'hidden', 'display' : 'none'});
+                 $("#fs").css({'visibility' : 'visible', 'display' : 'block'});
 
                 // extend button functionality
-                $('#fs').bind('touchstart', function() {
+                $('#fs').bind('touchstart click', function() {
                     // display the video
                     $("#myVideo").css({'visibility' : 'visible', 'display' : 'block'});
 
