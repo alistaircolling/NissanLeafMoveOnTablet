375baef743445c59b193c2414a18bd7b23a8f95c
dhrubo <dhrubo@gmail.com>
2013-07-01 12:12:19 +0100

    button touchevent added

--- js/video.js
+++ js/video.js
@@ -1,13 +1,18 @@
 
+
+
+
             $(function(){
                 // hide video
                 $("#myVideo").css({'visibility' : 'hidden', 'display' : 'none'});
 
                 // extend button functionality
-                $('#fs').bind('click', function() {
+                $('#fs').bind('touchstart', function() {
                     // display the video
                     $("#myVideo").css({'visibility' : 'visible', 'display' : 'block'});
 
+                    $("#fs").css({'visibility' : 'hidden', 'display' : 'none'});
+                    
                     // launch the video fullscreen
                     $("#myVideo")[0].webkitEnterFullscreen();
                 });
